0e51938631cb45a15f0e65cff84f88446794cf30
Jan Rabe <jan.rabe@exozet.com>
2019-03-26 18:35:20 +0100

    fixed another import

--- vrlib/src/main/java/com/asha/vrlib/MD360Renderer.java
+++ vrlib/src/main/java/com/asha/vrlib/MD360Renderer.java
@@ -21,6 +21,7 @@ import javax.microedition.khronos.opengles.GL10;
 import static com.asha.vrlib.common.GLUtil.glCheck;
 import java.nio.ShortBuffer;
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import android.graphics.Bitmap;
 
 /**
