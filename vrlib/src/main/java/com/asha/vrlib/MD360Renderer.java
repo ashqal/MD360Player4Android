53dd6913968eb7e55181c722554072a464e08b63
Jan Rabe <jan.rabe@exozet.com>
2019-03-26 18:17:06 +0100

    added screenshot method

--- vrlib/src/main/java/com/asha/vrlib/MD360Renderer.java
+++ vrlib/src/main/java/com/asha/vrlib/MD360Renderer.java
@@ -29,146 +29,189 @@ import static com.asha.vrlib.common.GLUtil.glCheck;
  */
 public class MD360Renderer implements GLSurfaceView.Renderer {
 
-	private static final String TAG = "MD360Renderer";
-	private DisplayModeManager mDisplayModeManager;
-	private ProjectionModeManager mProjectionModeManager;
-	private MDPluginManager mPluginManager;
-	private MDAbsLinePipe mMainLinePipe;
-	private MDGLHandler mGLHandler;
-	private Fps mFps = new Fps();
-	private int mWidth;
-	private int mHeight;
+    private static final String TAG = "MD360Renderer";
+    private DisplayModeManager mDisplayModeManager;
+    private ProjectionModeManager mProjectionModeManager;
+    private MDPluginManager mPluginManager;
+    private MDAbsLinePipe mMainLinePipe;
+    private MDGLHandler mGLHandler;
+    private Fps mFps = new Fps();
+    private int mWidth;
+    private int mHeight;
 
-	// private MDBarrelDistortionPlugin mBarrelDistortionPlugin;
+    // private MDBarrelDistortionPlugin mBarrelDistortionPlugin;
 
-	// final
-	private final Context mContext;
+    // final
+    private final Context mContext;
 
-	private MD360Renderer(Builder params){
-		mContext = params.context;
-		mDisplayModeManager = params.displayModeManager;
-		mProjectionModeManager = params.projectionModeManager;
-		mPluginManager = params.pluginManager;
-		mGLHandler = params.glHandler;
+    private MD360Renderer(Builder params) {
+        mContext = params.context;
+        mDisplayModeManager = params.displayModeManager;
+        mProjectionModeManager = params.projectionModeManager;
+        mPluginManager = params.pluginManager;
+        mGLHandler = params.glHandler;
 
-		mMainLinePipe = new MDBarrelDistortionLinePipe(mDisplayModeManager);
-	}
+        mMainLinePipe = new MDBarrelDistortionLinePipe(mDisplayModeManager);
+    }
 
-	@Override
-	public void onSurfaceCreated(GL10 glUnused, EGLConfig config){
-		// set the background clear color to black.
-		GLES20.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-		
-		// use culling to remove back faces.
-		GLES20.glEnable(GLES20.GL_CULL_FACE);
+    @Override
+    public void onSurfaceCreated(GL10 glUnused, EGLConfig config) {
+        // set the background clear color to black.
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
-		// enable depth testing
-		// GLES20.glEnable(GLES20.GL_DEPTH_TEST);
-	}
+        // use culling to remove back faces.
+        GLES20.glEnable(GLES20.GL_CULL_FACE);
 
-	@Override
-	public void onSurfaceChanged(GL10 glUnused, int width, int height){
-		this.mWidth = width;
-		this.mHeight = height;
+        // enable depth testing
+        // GLES20.glEnable(GLES20.GL_DEPTH_TEST);
+    }
 
-		mGLHandler.dealMessage();
-	}
+    @Override
+    public void onSurfaceChanged(GL10 glUnused, int width, int height) {
+        this.mWidth = width;
+        this.mHeight = height;
 
-	@Override
-	public void onDrawFrame(GL10 glUnused){
-		// gl thread
-		// 切换策略
-		// 热点拾取
-		mGLHandler.dealMessage();
+        mGLHandler.dealMessage();
+    }
 
-		GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
-		glCheck("MD360Renderer onDrawFrame begin. ");
+    @Override
+    public void onDrawFrame(GL10 glUnused) {
+        // gl thread
+        // 切换策略
+        // 热点拾取
+        mGLHandler.dealMessage();
 
-		int size = mDisplayModeManager.getVisibleSize();
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+        glCheck("MD360Renderer onDrawFrame begin. ");
 
-		int width = (int) (this.mWidth * 1.0f / size);
-		int height = mHeight;
+        int size = mDisplayModeManager.getVisibleSize();
 
-		// take over
-		mMainLinePipe.setup(mContext);
-		mMainLinePipe.takeOver(mWidth,mHeight,size);
+        int width = (int) (this.mWidth * 1.0f / size);
+        int height = mHeight;
 
-		List<MD360Director> directors = mProjectionModeManager.getDirectors();
+        // take over
+        mMainLinePipe.setup(mContext);
+        mMainLinePipe.takeOver(mWidth, mHeight, size);
 
-		// main plugin
-		MDAbsPlugin mainPlugin = mProjectionModeManager.getMainPlugin();
-		if (mainPlugin != null){
-			mainPlugin.setupInGL(mContext);
-			mainPlugin.beforeRenderer(this.mWidth, this.mHeight);
-		}
+        List<MD360Director> directors = mProjectionModeManager.getDirectors();
 
-		for (MDAbsPlugin plugin : mPluginManager.getPlugins()) {
-			plugin.setupInGL(mContext);
-			plugin.beforeRenderer(this.mWidth, this.mHeight);
-		}
+        // main plugin
+        MDAbsPlugin mainPlugin = mProjectionModeManager.getMainPlugin();
+        if (mainPlugin != null) {
+            mainPlugin.setupInGL(mContext);
+            mainPlugin.beforeRenderer(this.mWidth, this.mHeight);
+        }
 
-		for (int i = 0; i < size; i++){
-			if (i >= directors.size()) break;
+        for (MDAbsPlugin plugin : mPluginManager.getPlugins()) {
+            plugin.setupInGL(mContext);
+            plugin.beforeRenderer(this.mWidth, this.mHeight);
+        }
 
-			MD360Director director = directors.get(i);
-			GLES20.glViewport(width * i, 0, width, height);
-			GLES20.glEnable(GLES20.GL_SCISSOR_TEST);
-			GLES20.glScissor(width * i, 0, width, height);
+        for (int i = 0; i < size; i++) {
+            if (i >= directors.size()) break;
 
-			if (mainPlugin != null){
-				mainPlugin.renderer(i, width, height, director);
-			}
+            MD360Director director = directors.get(i);
+            GLES20.glViewport(width * i, 0, width, height);
+            GLES20.glEnable(GLES20.GL_SCISSOR_TEST);
+            GLES20.glScissor(width * i, 0, width, height);
 
-			for (MDAbsPlugin plugin : mPluginManager.getPlugins()) {
-				plugin.renderer(i, width, height, director);
-			}
+            if (mainPlugin != null) {
+                mainPlugin.renderer(i, width, height, director);
+            }
 
-			GLES20.glDisable(GLES20.GL_SCISSOR_TEST);
-		}
+            for (MDAbsPlugin plugin : mPluginManager.getPlugins()) {
+                plugin.renderer(i, width, height, director);
+            }
 
-		mMainLinePipe.commit(mWidth, mHeight, size);
-		glCheck("MD360Renderer onDrawFrame end. ");
-		// mFps.step();
-	}
+            GLES20.glDisable(GLES20.GL_SCISSOR_TEST);
+        }
 
-	public static Builder with(Context context) {
-		Builder builder = new Builder();
-		builder.context = context;
-		return builder;
-	}
+        mMainLinePipe.commit(mWidth, mHeight, size);
+        glCheck("MD360Renderer onDrawFrame end. ");
+        // mFps.step();
 
-	public static class Builder{
-		private Context context;
-		private DisplayModeManager displayModeManager;
-		private ProjectionModeManager projectionModeManager;
-		private MDGLHandler glHandler;
-		private MDPluginManager pluginManager;
+        if (screenshot) {
+            int screenshotSize = mViewportWidth * mViewportHeight;
+            ByteBuffer bb = ByteBuffer.allocateDirect(screenshotSize * 4);
+            bb.order(ByteOrder.nativeOrder());
+            gl.glReadPixels(0, 0, mViewportWidth, mViewportHeight, GL10.GL_RGBA, GL10.GL_UNSIGNED_BYTE, bb);
+            int pixelsBuffer[] = new int[screenshotSize];
+            bb.asIntBuffer().get(pixelsBuffer);
+            bb = null;
+            Bitmap bitmap = Bitmap.createBitmap(mViewportWidth, mViewportHeight, Bitmap.Config.RGB_565);
+            bitmap.setPixels(pixelsBuffer, screenshotSize - mViewportWidth, -mViewportWidth, 0, 0, mViewportWidth, mViewportHeight);
+            pixelsBuffer = null;
 
-		private Builder() {
-		}
+            short sBuffer[] = new short[screenshotSize];
+            ShortBuffer sb = ShortBuffer.wrap(sBuffer);
+            bitmap.copyPixelsToBuffer(sb);
 
-		public MD360Renderer build(){
-			return new MD360Renderer(this);
-		}
+            //Making created bitmap (from OpenGL points) compatible with Android bitmap
+            for (int i = 0; i < screenshotSize; ++i) {
+                short v = sBuffer[i];
+                sBuffer[i] = (short) (((v & 0x1f) << 11) | (v & 0x7e0) | ((v & 0xf800) >> 11));
+            }
+            sb.rewind();
+            bitmap.copyPixelsFromBuffer(sb);
+            lastScreenshot = bitmap;
+            screenshotListener.onScreenshot(lastScreenshot);
+            screenshotListener = null;
 
-		public Builder setGLHandler(MDGLHandler glHandler){
-			this.glHandler = glHandler;
-			return this;
-		}
+            screenshot = false;
+        }
+    }
 
-		public Builder setPluginManager(MDPluginManager pluginManager) {
-			this.pluginManager = pluginManager;
-			return this;
-		}
+    private volatile boolean screenshot = false;
 
-		public Builder setDisplayModeManager(DisplayModeManager displayModeManager) {
-			this.displayModeManager = displayModeManager;
-			return this;
-		}
+    private ScreenshotListener screenshotListener;
 
-		public Builder setProjectionModeManager(ProjectionModeManager projectionModeManager) {
-			this.projectionModeManager = projectionModeManager;
-			return this;
-		}
-	}
+    public static interface ScreenshotListener {
+        onScreenshot(Bitmap bitmap);
+    }
+
+    public void takeScreenshot(ScreenshotListener screenshotListener) {
+        this.screenshotListener = screenshotListener
+        screenshot = true;
+    }
+
+    public static Builder with(Context context) {
+        Builder builder = new Builder();
+        builder.context = context;
+        return builder;
+    }
+
+    public static class Builder {
+        private Context context;
+        private DisplayModeManager displayModeManager;
+        private ProjectionModeManager projectionModeManager;
+        private MDGLHandler glHandler;
+        private MDPluginManager pluginManager;
+
+        private Builder() {
+        }
+
+        public MD360Renderer build() {
+            return new MD360Renderer(this);
+        }
+
+        public Builder setGLHandler(MDGLHandler glHandler) {
+            this.glHandler = glHandler;
+            return this;
+        }
+
+        public Builder setPluginManager(MDPluginManager pluginManager) {
+            this.pluginManager = pluginManager;
+            return this;
+        }
+
+        public Builder setDisplayModeManager(DisplayModeManager displayModeManager) {
+            this.displayModeManager = displayModeManager;
+            return this;
+        }
+
+        public Builder setProjectionModeManager(ProjectionModeManager projectionModeManager) {
+            this.projectionModeManager = projectionModeManager;
+            return this;
+        }
+    }
 }
